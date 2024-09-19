816a0da3a9d98852d9efdfe2489f5c6ff3aa656e
Mohammad Zaid Khan <mzaidk222@gmail.com>
2024-09-19 18:15:10 +0530

    updated register page

--- Client/src/Pages/Register/Register.jsx
+++ Client/src/Pages/Register/Register.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom";
 import "./Register.css";
-import {ToastContainer, toast} from "react-toastify";
+import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
-import { host_url } from "../../utils/APIroutes";
+import { register_url } from "../../utils/APIroutes";
 
 const Register = () => {
   const navigate = useNavigate();
@@ -21,42 +21,44 @@ const Register = () => {
     pauseOnHover: true,
     draggable: true,
     theme: "dark",
-  }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if(handleValidation()) {
-      const {username, email, password} = formValues;
-      let {data} = await axios.post(`${host_url}/api/auth/register`, {
-        username, email, password
+    if (handleValidation()) {
+      const { username, email, password } = formValues;
+      let { data } = await axios.post(register_url, {
+        username,
+        email,
+        password,
       });
 
-      if(data.status === false) {
+      if (data.status === false) {
         toast.error(data.msg, toastOptions);
       } else {
         console.log(data.user);
         localStorage.setItem("chat-user", JSON.stringify(data.user));
-        navigate("/")
+        navigate("/");
       }
     }
   };
 
-  const handleValidation = ()=>{
-    const {username, email, password, confirm_password} = formValues;
-    if(password != confirm_password) {
+  const handleValidation = () => {
+    const { username, email, password, confirm_password } = formValues;
+    if (password != confirm_password) {
       toast.error("Confirm passeword did not match !", toastOptions);
       return false;
-    } else if(username.length < 4) {
+    } else if (username.length < 4) {
       toast.error("Username must be greater than 3 characters", toastOptions);
       return false;
-    } else if(email.length < 4) {
+    } else if (email.length < 4) {
       toast.error("email must be greater than 3 characters", toastOptions);
       return false;
     } else {
       return true;
     }
-  }
+  };
 
   const handleChange = (e) => {
     setFormValues({
@@ -67,53 +69,60 @@ const Register = () => {
 
   return (
     <>
-    <section
-      className="register flex items-center justify-center w-full h-screen"
-      onSubmit={handleSubmit}
-    >
-      <form className="md:w-1/2 w-full md:h-4/5 h-full bg-black rounded-3xl flex flex-col items-center justify-center gap-10 md:px-28">
-        <input
-          type="text"
-          id="username"
-          placeholder="Username"
-          className="form-inputs"
-          onChange={handleChange}
-          value={formValues.username}
-        />
+      <section className="register flex items-center justify-center w-full h-screen">
+        <form
+          className="md:w-1/2 w-full md:h-4/5 h-full bg-black rounded-3xl flex flex-col items-center justify-center gap-10 md:px-28"
+          onSubmit={handleSubmit}
+        >
+          <input
+            type="text"
+            id="username"
+            placeholder="Username"
+            className="form-inputs"
+            onChange={handleChange}
+            value={formValues.username}
+          />
 
-        <input
-          type="email"
-          id="email"
-          placeholder="Email"
-          className="form-inputs"
-          onChange={handleChange}
-          value={formValues.email}
-        />
+          <input
+            type="email"
+            id="email"
+            placeholder="Email"
+            className="form-inputs"
+            onChange={handleChange}
+            value={formValues.email}
+          />
 
-        <input
-          type="password"
-          id="password"
-          placeholder="Password"
-          className="form-inputs"
-          onChange={handleChange}
-          value={formValues.password}
-        />
+          <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            className="form-inputs"
+            onChange={handleChange}
+            value={formValues.password}
+          />
 
-        <input
-          type="password"
-          id="confirm_password"
-          placeholder="Confirm Password"
-          className="form-inputs"
-          onChange={handleChange}
-          value={formValues.confirm_password}
-        />
+          <input
+            type="password"
+            id="confirm_password"
+            placeholder="Confirm Password"
+            className="form-inputs"
+            onChange={handleChange}
+            value={formValues.confirm_password}
+          />
 
-        <button type="submit" className="submit-btn">
-          Register
-        </button>
-      </form>
-      <ToastContainer />
-    </section>
+          <button type="submit" className="submit-btn">
+            Register
+          </button>
+
+          <p className="text-center text-gray-400">
+            Already have an account? 
+            <Link to={"/login"} className="text-blue-700 ps-2">
+              Login
+            </Link>
+          </p>
+        </form>
+        <ToastContainer />
+      </section>
     </>
   );
 };
