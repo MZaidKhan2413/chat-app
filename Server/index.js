969eb7f3a725778659bdbaf311d3f025281c1c1e
Mohammad Zaid Khan <mzaidk222@gmail.com>
2024-09-22 11:15:46 +0530

    changed cors origin

--- Server/index.js
+++ Server/index.js
@@ -30,7 +30,7 @@ const server = app.listen(PORT, ()=>{
 
 const io = socket(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: "https://vibey-chat-app.vercel.app",
       credentials: true
     }
 });
